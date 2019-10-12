1be03275f81114715a5045b9b176403cf3d3f9df
Katie Rose <katiefelten@gmail.com>
2019-10-12 17:48:57 -0400

    :zap: updated search screen

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, ScrollView } from "react-native";
 import SearchBar from "../components/SearchBar";
-import yelp from "../api/yelp";
 import useResults from "../hooks/useResults";
 import ResultsList from "../components/ResultsList";
 
@@ -9,31 +8,33 @@ const SearchScreen = () => {
   const [term, setTerm] = useState("");
   const [searchApi, results, errorMessage] = useResults();
 
-  const searchApi = async () => {
-    try {
-      const response = await yelp.get("/search", {
-        params: {
-          limit: 50,
-          term,
-          location: "Atlanta"
-        }
-      });
-      setResults(response.data.businesses);
-    } catch (err) {
-      console.log(err);
-    }
+  const filterResultsByPrice = price => {
+    // price === '$' || '$$' || '$$$'
+    return results.filter(result => {
+      return result.price === price;
+    });
   };
 
   return (
-    <View>
+    <>
       <SearchBar
         term={term}
         onTermChange={setTerm}
         onTermSubmit={() => searchApi(term)}
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
-      <Text>We have found {results.length} results</Text>
-    </View>
+      <ScrollView>
+        <ResultsList
+          results={filterResultsByPrice("$")}
+          title='Cost Effective'
+        />
+        <ResultsList results={filterResultsByPrice("$$")} title='Bit Pricier' />
+        <ResultsList
+          results={filterResultsByPrice("$$$")}
+          title='Big Spender'
+        />
+      </ScrollView>
+    </>
   );
 };
 
